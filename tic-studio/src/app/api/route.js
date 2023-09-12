54886e2fd68c95620ebdaaf874cc0755a30603ef
Darya <daryabsb@gmail.com>
2023-09-12 10:10:08 +0300

    transfered tic-studio to its own repo

--- tic-studio/src/app/api/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return NextResponse.json({hello: "World"})
-}
-
-export async function POST() {
-    return NextResponse.json({hello: "Post"})
-}
